f4d72fdefd4e343d36dbd972d12706ba0cdc8598
Timofei Ustyantsev <ustyantsevxx@gmail.com>
2022-04-26 16:48:58 +0500

    Refactor

--- vars/sendTelegramMessage.groovy
+++ vars/sendTelegramMessage.groovy
@@ -1,12 +1,13 @@
-def call(message) {
+void call(String rawMessage) {
     withCredentials([
         string(credentialsId: 'telegramBotToken', variable: 'TOKEN'),
-        string(credentialsId: 'telegramChatId', variable: 'CHAT_ID')
+     string(credentialsId: 'telegramChatId', variable: 'CHAT_ID')
     ]) {
         sh """
         curl -G 'https://api.telegram.org/bot$TOKEN/sendMessage' \
             --data-urlencode "chat_id=$CHAT_ID" \
-            --data-urlencode "text=$message"
+            --data-urlencode "text=$rawMessage"
         """
-    }  
+    }
 }
+
