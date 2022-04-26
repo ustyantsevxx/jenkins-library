baea62b6af96bd46a6ee789618724aed1683aba2
Timofei Ustyantsev <ustyantsevxx@gmail.com>
2022-04-26 16:26:18 +0500

    asdf

--- vars/sendTelegramMessage.groovy
+++ vars/sendTelegramMessage.groovy
@@ -1,14 +1,7 @@
 def call(message) {
     def encodedMessage = URLEncoder.encode(message, "UTF-8")
-
     withCredentials([string(credentialsId: 'telegramBotToken', variable: 'TOKEN'),
     string(credentialsId: 'telegramChatId', variable: 'CHAT_ID')]) {
-
-        response = httpRequest (consoleLogResponseBody: true,
-                contentType: 'APPLICATION_JSON',
-                httpMode: 'GET',
-                url: "https://api.telegram.org/bot$TOKEN/sendMessage?text=$encodedMessage&chat_id=$CHAT_ID&disable_web_page_preview=true",
-                validResponseCodes: '200')
-        return response
+        sh "curl -G https://api.telegram.org/bot$TOKEN/sendMessage?text=$encodedMessage&chat_id=$CHAT_ID&disable_web_page_preview=true"
     }
 }
