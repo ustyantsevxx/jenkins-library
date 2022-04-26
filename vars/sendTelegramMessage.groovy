786ddb0cbe28ef0f60b5641560f8a11bb7555fd6
Timofei Ustyantsev <ustyantsevxx@gmail.com>
2022-04-26 16:34:00 +0500

    Lint

--- vars/sendTelegramMessage.groovy
+++ vars/sendTelegramMessage.groovy
@@ -1,10 +1,12 @@
 def call(message) {
-    withCredentials([string(credentialsId: 'telegramBotToken', variable: 'TOKEN'),
-    string(credentialsId: 'telegramChatId', variable: 'CHAT_ID')]) {
+    withCredentials([
+        string(credentialsId: 'telegramBotToken', variable: 'TOKEN'),
+        string(credentialsId: 'telegramChatId', variable: 'CHAT_ID')
+    ]) {
         sh """
         curl -G 'https://api.telegram.org/bot$TOKEN/sendMessage' \
             --data-urlencode "chat_id=$CHAT_ID" \
             --data-urlencode "text=$message"
- """
-    }
+        """
+    }  
 }
